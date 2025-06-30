cd4299983a717d47796f6deee876979391416161
AlejandroGarciaBautista <alu0101387604@ull.edu.es>
2025-06-30 18:58:43 +0000

    BGP EVPN Files & BGP EVPN Tests

---
+++ BGP-EVPN/delay_adder.py
@@ -0,0 +1,37 @@
+import yaml
+import subprocess
+
+# Cargar el fichero topology.yml
+with open("topology.yml", "r") as file:
+    data = yaml.safe_load(file)
+
+links = data['topology']['links']
+
+# delay = "10ms"
+delay = "30ms"
+
+for link in links:
+    ep1, ep2 = link['endpoints']
+    
+    node1, iface1 = ep1.split(":")
+    node2, iface2 = ep2.split(":")
+
+    cmd1 = [
+    "containerlab", "tools", "netem", "set",
+    "-n", f"clab-tfg-frr-ansible-{node1}",
+    "-i", iface1,
+    "--delay", delay
+    ]
+
+    print(f"Ejecutando: {' '.join(cmd1)}")
+    subprocess.run(cmd1)
+
+    # cmd2 = [
+    # "containerlab", "tools", "netem", "set",
+    # "-n", f"clab-tfg-frr-ansible-{node2}",
+    # "-i", iface2,
+    # "--delay", delay
+    # ]
+
+    # print(f"Ejecutando: {' '.join(cmd2)}")
+    # subprocess.run(cmd2)
---
+++ resultados/extract.py
@@ -0,0 +1,28 @@
+import os
+import re
+import json
+
+directorio_logs = "."  
+
+
+resultados = {}
+
+# Expresión regular para extraer los valores de time=... ms
+regex_time = re.compile(r'time=([\d.]+)\s*ms')
+
+# Recorrer todos los ficheros .log en el directorio
+for archivo in os.listdir(directorio_logs):
+    if archivo.endswith(".log"):
+        tiempos = []
+        ruta_fichero = os.path.join(directorio_logs, archivo)
+        with open(ruta_fichero, "r") as f:
+            for linea in f:
+                match = regex_time.search(linea)
+                if match:
+                    tiempos.append(float(match.group(1)))
+        resultados[archivo] = tiempos
+
+with open("datos.json", "w") as f_json:
+    json.dump(resultados, f_json, indent=4)
+
+print("Resultados guardados en 'datos.json'")
